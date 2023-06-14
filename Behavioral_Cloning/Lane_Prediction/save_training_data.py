2a2f137213c74c227a93f952a73c9d2bcba9ce86
choudhury722k <soumyaranjanchoudhury194@gmail.com>
2023-06-14 23:55:23 +0530

    Behavioral_cloning_training

---
+++ Behavioral_Cloning/Lane_Prediction/save_training_data.py
@@ -0,0 +1,39 @@
+import cv2
+from Predict_steering import process_pipeline
+from Camera_callibration import calibrate_camera
+
+def save_image_and_steering_angle(video_file):
+
+    _, mtx, dist, _, _ = calibrate_camera(calib_images_dir='camera_cal')
+    curr_steering_angle = 90
+
+    cap = cv2.VideoCapture(video_file)
+    try:
+        i = 0
+        while cap.isOpened():
+            return_value, frame = cap.read()
+
+            if return_value:
+                pass
+            else:
+                print('Video has ended or failed, try a different video format!')
+                break
+
+            blend, curr_steering_angle = process_pipeline(frame, mtx, dist, curr_steering_angle, keep_state=False)
+            # print("/home/soumya/Self-Driving-Car/Behavioral_Cloning/Training/steering_data/image/%s_%03d_%03d.png" % (video_file, i, curr_steering_angle))
+            cv2.imwrite("/home/soumya/Self-Driving-Car/Behavioral_Cloning/Training/steering_data/image/%s_%03d_%03d.png" % (video_file, i, curr_steering_angle), frame)
+
+            i += 1
+
+            cv2.imshow('Frame',blend)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    video_path = "project_video.mp4"
+    # video_path = 0
+    save_image_and_steering_angle(video_path)
