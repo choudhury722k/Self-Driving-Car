48348e8d0bb63366f025cf2020fa8f36168858dd
choudhury722k <soumyaranjanchoudhury194@gmail.com>
2023-06-20 01:41:46 +0530

    Testing behavioral cloning on video

--- Behavioral_Cloning/Testing/drive.py
+++ Behavioral_Cloning/Testing/drive.py
@@ -1,139 +1,68 @@
-import argparse
-import base64
-from datetime import datetime
-import os
-import shutil
+import cv2
 
-import numpy as np
-import socketio
-import eventlet
-import eventlet.wsgi
-from PIL import Image
-from flask import Flask
-from io import BytesIO
-
+# tensorflow
+import tensorflow as tf
+import keras
+from keras.models import Sequential  # V2 is tensorflow.keras.xxxx, V1 is keras.xxx
+from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
+from keras.optimizers import Adam
 from keras.models import load_model
-import h5py
-from keras import __version__ as keras_version
 
-sio = socketio.Server()
-app = Flask(__name__)
-model = None
-prev_image_array = None
+print( f'tf.__version__: {tf.__version__}' )
+print( f'keras.__version__: {keras.__version__}' )
 
+def nvidia_model():
+    model = Sequential(name='Nvidia_Model')
+    
+    # elu=Expenential Linear Unit, similar to leaky Relu
+    # skipping 1st hiddel layer (nomralization layer), as we have normalized the data
+    
+    # Convolution Layers
+    model.add(Conv2D(24, (5, 5), strides=(2, 2), input_shape=(66, 200, 3), activation='elu')) 
+    model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='elu')) 
+    model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='elu')) 
+    model.add(Conv2D(64, (3, 3), activation='elu')) 
+    model.add(Dropout(0.2)) # not in original model. added for more robustness
+    model.add(Conv2D(64, (3, 3), activation='elu')) 
+    
+    # Fully Connected Layers
+    model.add(Flatten())
+    model.add(Dropout(0.2)) # not in original model. added for more robustness
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    
+    # output layer: turn angle (from 45-135, 90 is straight, <90 turn left, >90 turn right)
+    model.add(Dense(1)) 
+    
+    # since this is a regression problem not classification problem,
+    # we use MSE (Mean Squared Error) as loss function
+    optimizer = Adam(lr=1e-3) # lr is learning rate
+    model.compile(loss='mse', optimizer=optimizer)
+    
+    return model
 
-class SimplePIController:
-    def __init__(self, Kp, Ki):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.set_point = 0.
-        self.error = 0.
-        self.integral = 0.
-
-    def set_desired(self, desired):
-        self.set_point = desired
-
-    def update(self, measurement):
-        # proportional error
-        self.error = self.set_point - measurement
-
-        # integral error
-        self.integral += self.error
-
-        return self.Kp * self.error + self.Ki * self.integral
-
-
-controller = SimplePIController(0.1, 0.002)
-set_speed = 9
-controller.set_desired(set_speed)
-
-
-@sio.on('telemetry')
-def telemetry(sid, data):
-    if data:
-        # The current steering angle of the car
-        steering_angle = data["steering_angle"]
-        # The current throttle of the car
-        throttle = data["throttle"]
-        # The current speed of the car
-        speed = data["speed"]
-        # The current image from the center camera of the car
-        imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
-
-        throttle = controller.update(float(speed))
-
-        print(steering_angle, throttle)
-        send_control(steering_angle, throttle)
-
-        # save frame
-        if args.image_folder != '':
-            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
-            image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
-    else:
-        # NOTE: DON'T EDIT THIS.
-        sio.emit('manual', data={}, skip_sid=True)
+model = nvidia_model()
+print(model.summary())
 
 
-@sio.on('connect')
-def connect(sid, environ):
-    print("connect ", sid)
-    send_control(0, 0)
+path = "lane_navigation_final.h5"
+# # path.encode().decode("utf-8")
+# model.load_weights(path)
 
+cap = cv2.VideoCapture('/home/soumya/Self-Driving-Car/Behavioral_Cloning/Video_data/test_video.mp4')
 
-def send_control(steering_angle, throttle):
-    sio.emit(
-        "steer",
-        data={
-            'steering_angle': steering_angle.__str__(),
-            'throttle': throttle.__str__()
-        },
-        skip_sid=True)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Remote Driving')
-    parser.add_argument(
-        'model',
-        type=str,
-        help='Path to model h5 file. Model should be on the same path.'
-    )
-    parser.add_argument(
-        'image_folder',
-        type=str,
-        nargs='?',
-        default='',
-        help='Path to image folder. This is where the images from the run will be saved.'
-    )
-    args = parser.parse_args()
-
-    # check that model Keras version is same as local Keras version
-    f = h5py.File(args.model, mode='r')
-    model_version = f.attrs.get('keras_version')
-    keras_version = str(keras_version).encode('utf8')
-
-    if model_version != keras_version:
-        print('You are using Keras version ', keras_version,
-              ', but the model was built using ', model_version)
-
-    model = load_model(args.model)
-
-    if args.image_folder != '':
-        print("Creating image folder at {}".format(args.image_folder))
-        if not os.path.exists(args.image_folder):
-            os.makedirs(args.image_folder)
-        else:
-            shutil.rmtree(args.image_folder)
-            os.makedirs(args.image_folder)
-        print("RECORDING THIS RUN ...")
-    else:
-        print("NOT RECORDING THIS RUN ...")
-
-    # wrap Flask application with engineio's middleware
-    app = socketio.Middleware(sio, app)
-
-    # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+while True:
+    ret, frame = cap.read()
+    if not ret: break
+    
+    cv2.imshow("frame", frame)
+    # Y_pred = model.predict(frame)
+    # print(Y_pred)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+# release and destroy windows
+cap.release()
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Object detection and tracing for detcting cars and traffic lights and take decision accordingly
 
-Behavioral cloning for detecting the tracks now i have trained with only steering angles more features to be added in future works
+Behavioral cloning for detecting the tracks now i have trained with only steering angles more features to be added in future works also train with more data of steering angles
+
